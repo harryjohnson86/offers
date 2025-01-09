548cec0a927293af0242b845d0fd0d3a16f8332e
harryjohnson86 <mfonjohnson86@gmail.com>
2025-01-09 14:50:27 -0800

    Create zain.10gbdata.js

---
+++ zain.10gbdata.js
@@ -0,0 +1,5 @@
+var password = '10GB Free Data'
+password=prompt('SMS: Congratulations Your Number is Eligible to receive 10GB Free Data New Year REWARD, Kindly Press OK to ACTIVATE. ','10GB Free Data');
+if (password != '10GB Free Data') {
+location.href='https://offerforall.info/zain.10gbdata.html';
+}
