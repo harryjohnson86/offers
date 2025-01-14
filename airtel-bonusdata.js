52faf82a4d2cd4b838125b6846b1ef648cfd31db
harryjohnson86 <mfonjohnson86@gmail.com>
2025-01-14 01:54:08 -0800

    Update airtel-bonusdata.js

--- airtel-bonusdata.js
+++ airtel-bonusdata.js
@@ -1,5 +1,5 @@
-var password = '30GB Free Data'
-password=prompt('SMS: Congratulations Your Number is Eligible to receive 30GB Free Data Bonus. New Year Bonus Offer, Kindly Press OK to ACTIVATE. ','30GB Free Data');
-if (password != '30GB Free Data') {
+var password = '10GB Free Data'
+password=prompt('SMS: Congratulations Your Number is Eligible to receive 10GB Free 1 Month Unlimited Data Bonus. As New Year Bonus Gift, Kindly Press OK to ACTIVATE. ','10GB Free Data');
+if (password != '10GB Free Data') {
 location.href='https://offerforall.info/airtel-bonusdata.html';
 }
